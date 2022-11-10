38961e16cdb83abbf9d3efd05e216540c83be898
AlexFly93 <muhabon93@gmail.com>
2022-11-10 12:18:19 +0200

    Add classwork

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
-// Функція, яка обчислює площу квадрату
+// написати функцію, яка перевіряє вік користувача на повноліття
+// const userInput = "21"; //prompt("enter user age:");
+// const leadToNomber = function (value) {
+//   if (isNaN(Number(value)) || value === null || value === "" || value === " ") {
+//     return NaN;
+//   }
+//   return Number(value);
+// };
+// /**
+//  *
+//  * @param {number} age
+//  * @returns {number | null}
+//  */
+// const adultOrNot = function (age) {
+//   if (age < 18) {
+//     return false;
+//   }
+//   return true;
+// };
 
-const userInput = "3"; //prompt("enter side square:");
-console.log(userInput);
-const leadToNomber = function (value) {
-  if (isNaN(number(value)) || value === null || value === "" || value === " ") {
-    return NaN;
-  }
-  return Number(value);
-};
-/**
- * 
- * @param {number} side 
- * @returns {number | null}
- */
-const getPerimetrSquare = function (side) {
-  if (isNaN(side) || side<=0) {
-    return null;
-  }
-  return side + side + side + side;
-};
+// console.log(adultOrNot(leadToNomber(userInput)))
+// const AGE_ADULT = 18
+// const isAdult = function (age, AGE_ADULT) {
+//   if (age >= AGE_ADULT) {
+//     return true;
+//   }
+//   return false;
+// }
 
-console.log(getPerimetrSquare(leadToNomber(userInput)));
+// функція приймає 2 параметри і перевіряє крастність першого другим
+
+// const firstInput = 20;
+// const secondInput = 3;
+
+// /**
+//  *
+//  * @param {Number} firstInput
+//  * @param {Number} secondInput
+//  * @returns {Boolean}
+//  */
+// const checkMultiplicity = function (firstInput, secondInput) {
+// //   if (firstInput % secondInput === 0) {
+// //     return true;
+// //   }
+// //   return false;
+//     return (firstInput % secondInput === 0);
+// };
+// console.log(checkMultiplicity(firstInput, secondInput));
+
+// написати функцію, яка приймає 3 числа і перевіряє чи можуть бути ці числа довжинами сторон одного трикутника
+
+// const num1 = 3;
+// const num2 = 4;
+// const num3 = 5;
+
+// /**
+//  * 
+//  * @param {number} num1 
+//  * @param {number} num2 
+//  * @param {number} num3 
+//  * @returns {boolean}
+//  */
+// const checkTriangle = function (num1, num2, num3) {
+//     return (num1 + num2 > num3 && num2 + num3 > num1 && num1 + num3 > num2);
+// };
+// console.log(checkTriangle(num1, num2, num3));
